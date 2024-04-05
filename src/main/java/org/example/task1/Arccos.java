507aa3b221179c77117d6f8d9b3799e1aff20a22
artemiy <Ntvrfntvrf20031905!>
2024-04-05 10:46:28 +0300

    krasivo

--- src/main/java/org/example/task1/Arccos.java
+++ src/main/java/org/example/task1/Arccos.java
@@ -2,31 +2,36 @@ package org.example.task1;
 
 public class Arccos {
 
-    public static double Marcsin(double x0){
+    public static double Marcsin(double x0) {
         double x = x0;
-        if(x0 < 0){x =- x0;}
+        if (x0 < 0) {
+            x = -x0;
+        }
         double y = x;
         int n = 1;
         double sn = x;
-        while(true){
-            if (sn < 1E-7){
+        while (true) {
+            if (sn < 1E-7) {
                 break;
             }
             sn = sn * (2 + 1.0 / n) * 0.5 * x * x;
             y = y + sn / (2 * n + 1) / (2 * n + 1);
             n = n + 1;
         }
-        if(x0 < 0){y = -y;}
+        if (x0 < 0) {
+            y = -y;
+        }
         return y;
     }
-    public static double calc(double x){
-        if (x < -1 || x > 1){
+
+    public static double calc(double x) {
+        if (x < -1 || x > 1) {
             throw new IllegalArgumentException();
         } else if (x == 1) {
             return 0;
-        } else if (x == -1){
+        } else if (x == -1) {
             return Math.PI;
         }
-        return Math.PI/2 - Marcsin(x);
+        return Math.PI / 2 - Marcsin(x);
     }
 }
